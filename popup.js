43d9c3eff41ebfb29d09d815051a821601616d0f
rpontell <165290816+rpontell@users.noreply.github.com>
2024-07-01 17:44:49 +0200

    bugfix in data calculation

--- popup.js
+++ popup.js
@@ -14,7 +14,12 @@ document.addEventListener('DOMContentLoaded', function() {
         const title = article.querySelector('._article-title_1aaqi_4').textContent;
         const publishedDaysAgo = parseInt(article.querySelector('._taxonomy-item__time_al3wh_37.col-lg-1.col-sm-12').textContent);
         const publicationDate = new Date(today);
-        publicationDate.setDate(today.getDate() - publishedDaysAgo); // Sottrai giorni dalla data corrente
+        if (article.querySelector('._taxonomy-item__time_al3wh_37.col-lg-1.col-sm-12').textContent.includes('ore') || article.querySelector('._taxonomy-item__time_al3wh_37.col-lg-1.col-sm-12').textContent.includes('ora')) {
+          publicationDate.setDate(today.getDate());
+        } else {
+          publicationDate.setDate(today.getDate() - publishedDaysAgo); // Sottrai giorni dalla data corrente
+        }
+        
         const contentElement = article.querySelector('._taxonomy-item__content_al3wh_53.col-lg-7.col-8');
         const paragraphs = Array.from(contentElement.querySelectorAll('._article-paragraph_e98aq_1')).map(p => p.textContent).join('\n');
         const articleUrl = article.querySelector('._taxonomy-item_al3wh_1._opener_al3wh_14 a').getAttribute('href'); // URL dell'articolo completo
